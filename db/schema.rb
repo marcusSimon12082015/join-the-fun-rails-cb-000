829df55ed09694963a11462b8629053b28826410
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-10-07 15:15:53 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20181007150510) do
 
-  create_table "passengers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "taxis", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
