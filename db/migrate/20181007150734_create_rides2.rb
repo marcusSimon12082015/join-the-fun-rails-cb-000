4bb94f2b14da2780b6fad93db9168267cf02b799
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-10-07 15:09:00 +0000

    automatically backed up by learn

--- db/migrate/20181007150734_create_rides2.rb
+++
@@ -1,8 +0,0 @@
-class CreateTable < ActiveRecord::Migration
-  def change
-    create_table :rides do |t|
-      t.belongs_to :taxi, index: true
-      t.belongs_to :passenger, index: true
-    end
-  end
-end
